1f59f74a4b014b09dfbbb0c1fdce1f828b22f4e7
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-08-12 21:05:12 +0300

    error corrected

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -4,12 +4,13 @@ import css from "./SearchBox.module.css";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
+  const filterValue = useSelector(selectNameFilter);
   return (
     <div className={css.searchBox}>
       <p className={css.title}>Find contacts by name</p>
       <input
         className={css.input}
-        value={useSelector(selectNameFilter)}
+        value={filterValue}
         onChange={(e) => dispatch(changeFilter(e.target.value))}
       ></input>
     </div>
